a3d0db6dc59dd3ee333f85c253e61f4e406beb4d
Shaun Ready <readysd@gmail.com>
2015-05-25 13:03:56 -0300

    1

---
+++ application/controllers/Main.php
@@ -0,0 +1,27 @@
+<?php
+class Main extends CI_Controller {
+
+		public function __construct()
+        {
+                parent::__construct();
+                $this->load->model('Sections_Model');
+        }
+
+        public function view($page = 'home')
+		{
+	        if ( ! file_exists(APPPATH.'/views/pages/'.$page.'.php'))
+	        {
+	                // Whoops, we don't have a page for that!
+	                show_404();
+	        }
+
+	        $data['app_name'] = 'TactiCal';
+	        $data['sections'] = $this->Sections_Model->get_sections();
+	        $sectionID = $this->Sections_Model->getSectionID('Acute Care');
+	        $data['subSections'] = $this->Sections_Model->getSubSections($sectionID);
+
+	        $this->load->view('templates/header', $data);
+	        $this->load->view('pages/'.$page, $data);
+	        $this->load->view('templates/footer', $data);
+        }
+}
